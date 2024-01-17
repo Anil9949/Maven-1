04176380b8324d669e749fe89b756a86c63eb37d
Anil <nagulaanil92@gmail.com>
2024-01-17 15:46:10 +0530

    maven commit

---
+++ src/main/java/com/mithunsh/www/App.java
@@ -0,0 +1,13 @@
+package com.mithunsh.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
